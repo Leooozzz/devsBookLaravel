d55f49047bd346af0f7b015e7be3452381985e93
Leonardo De Souza <leozeraffcanal2405@gmail.com>
2026-04-12 18:10:16 -0300

    create a user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
 class AuthController extends Controller
 {
 
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login', 'create', 'unauthorized']]);
+    }
+    public function create(Request $request)
+    {
+
+        $array = ['error'=>''];
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $birthday = $request->input('birthday');
+
+        if ($name && $email && $password && $birthday) {
+            if (strtotime($birthday) === false) {
+                $array['error'] = "Invalid birthdate";
+                return $array;
+            }
+            $email_exists = User::where('email', $email)->count();
+            if ($email_exists === 0) {
+                $hash = Hash::make($password);
+
+                $new_user = new User();
+
+                $new_user->name = $name;
+                $new_user->email = $email;
+                $new_user->password = $hash;
+                $new_user->birthday = $birthday;
+
+                $new_user->save();
+        
+                $array['user'] = $new_user;
+                
+            } else {
+                $array['error'] = "email already registered";
+                return $array;
+            }
+        } else {
+            $array['error'] = 'did not send all fields';
+            return $array;
+        }
+        return $array;
     }
 }
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/user', [AuthController::class, 'create']);
+
+Route::get('/ping', function () {
+    return ['pong' => true];
+});
+
+/*
+Route::get('/401', 'AuthController@unathorized')->name('login');
+
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/auth/logout', 'AuthController@logout');
+Route::post('/auth/refresh', 'AuthController@refresh');
+*/
+Route::post('/user', [AuthController::class, 'create']);
+/*
+Route::put('/user', 'UserController@update');
+Route::post('/user/avatar', 'UserController@updateAvatar');
+Route::post('/user/cover', 'UserController@updateCover');
+
+Route::get('/feed', 'FeedController@read');
+Route::get('/user/feed', 'FeedController@userFeed');
+Route::get('/user/{id}/feed', 'FeedController@userFeed');
+
+Route::get('/user', 'UserController@read');
+Route::get('/user/:{id}', 'UserController@read');
+
+Route::post('/feed', 'FeedController@create');
+
+Route::post('/post/:{id}/like', 'PostController@like');
+Route::post('/post/:{id}/commemnt', 'PostController@comment');
+
+Route::get('/search', 'SearchController@search');
+*/
